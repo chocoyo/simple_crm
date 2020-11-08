eeb748f1d6ee89fcea3173b19c5db9e4c85f52ab
Mike Hodges <mikehodges7@outlook.com>
2020-11-07 19:25:40 -0600

    Added Routes And Controller Actions For Customers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "customers/index"
+  get "/customers/", to: "customers#index"
+  get "customers/alphabetized", to: "customers#alphabetized", as: "customersalphabetized"
+  get "customers/missing_email", to: "customers#missing_email", as: "customerswithoutemail"
 end
